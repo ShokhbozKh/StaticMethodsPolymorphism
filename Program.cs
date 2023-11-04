098d853d1e1568bd2d448bf1e1fbbbdf1f7b2efc
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-04 19:40:11 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator results = new Calculator();
+            Console.WriteLine(Calculator.Add(1,2));
+            Console.WriteLine(Calculator.Sub(12.21m,32));
+            Console.WriteLine(Calculator.Multiply(21,12.2m));
+            Console.WriteLine(Calculator.Div(12,5));
+            Console.WriteLine(Calculator.Abs(-343.3m));
+            Console.WriteLine(Calculator.Pow(12));
+
+        }
+    }
+    class Calculator
+    {
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static decimal Sub(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public static decimal Multiply(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        public static decimal Div(decimal a, decimal b)
+        {
+            return a / b;
+        }
+        public static decimal Abs(decimal a)//
+        {
+            if(a < 0)
+            {
+                return -a;
+            }
+            else
+            {
+                return a;
+            }
+        }
+        public static decimal Pow(decimal a)
+        {
+            return a * a;
+        }
+    }
+}
